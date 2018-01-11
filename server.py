bc2d46b3037a3a5ebc7338fd1af3b9a3c14e15b3
Cédric Picard <cedric.picard@digitalsecurity.lu>
2018-01-11 18:41:09 +0100

    First commit, base working

---
+++ server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import socketserver
+
+class CmdHttpHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(2**14).strip().decode("UTF-8")
+
+        if len(data) == 0:
+            return
+
+        elif self.data.splitlines()[0].startswith("GET"):
+            command = input("%s > " % self.client_address[0]).encode("UTF-8")
+
+            response = (b"HTTP/1.1 200\ncontent-length: "
+                        + str(len(command)).encode("UTF-8")
+                        + b"\n\n"
+                        + command)
+
+            self.request.sendall(response)
+
+
+        elif self.data.splitlines()[0].startswith("POST"):
+            data = self.request.recv(2**14).strip().decode("UTF-8")
+            print(data)
+            print()
+
+            response = (b"HTTP/1.1 200\ncontent-length: 0\n\n")
+            self.request.sendall(response)
+            return
+
+
+        else:
+            print(self.data.decode("UTF-8"))
+            response = (b"HTTP/1.1 300\ncontent-length: 0\n\n")
+            self.request.sendall(response)
+
+
+def main():
+    print("To close connection enter 'EXIT'")
+    print("The computer may be stalled by some commands, just try again")
+    print()
+
+    with socketserver.TCPServer(("0.0.0.0", 1338), CmdHttpHandler) as server:
+        server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
